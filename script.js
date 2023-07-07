74986be8ddd0c7ff400f27fe54a7b6956ba0133e
Sayan Dutta <100032007+Sayan-Dutta-2003@users.noreply.github.com>
2023-07-07 12:19:24 +0530

    Add files via upload
    HTML and JavaScript fundamentals

---
+++ script.js
@@ -0,0 +1,37 @@
+// var a = 20
+// var b = 30
+
+// function message() {
+//     // alert("Hello")
+//     // window.alert(a + b)   
+//     document.write(a + b)
+// }
+
+// function printa()
+// {
+//    document.write(a); 
+// }
+
+// function printb()
+// {
+//     document.write(b);
+// }
+
+// printa();
+// printb();
+
+function message()
+{
+    var num1 = parseInt(prompt('Enter The First Number: '));
+    var num2 = parseInt(prompt('Enter The FirsSecond Number: '));
+    var sum = num1 + num2;
+    document.write('The sum is: ' + sum)
+}
+
+
+
+
+
+
+
+
